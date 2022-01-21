303cb1b0f5378a0c1fad329371dda5282c2d4b36
mogooee <mogoo22@naver.com>
2022-01-21 21:36:38 +0900

    rename, refact: reduce 메소드 사용

--- cs10/0120/classifierAlpha.js
+++ cs10/0120/classifierAlpha.js
@@ -1,25 +1,16 @@
-const add = (a, b) => a + b;
-
-const reduce = (f, acc, iter) => {
-  for (const a of iter) {
-    acc = f(acc, a);
-  }
-  return acc;
-};
-
 const makeArray = (number) => {
   return Array.from({ length: number }, (x, i) => i + 1);
 };
 
 const factors = (number) => {
-  return reduce(
-    add,
-    0,
-    makeArray(number).filter((e) => number % e == 0)
-  );
+  return makeArray(number)
+    .filter((e) => number % e == 0)
+    .reduce((acc, cur) => {
+      return acc + cur;
+    });
 };
 
-module.exports.classifierAlpha = (number) => {
+module.exports.classifyAlpha = (number) => {
   const result = factors(number) - number;
   return result == number
     ? "perfect"
