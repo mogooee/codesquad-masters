6cd17188b55844a2a4f5f4f3b248fd36a2b81d63
mogooee <mogoo22@naver.com>
2022-01-23 00:07:08 +0900

    style: x축 변경

--- cs10/0113/graph.js
+++ cs10/0113/graph.js
@@ -4,7 +4,7 @@ const map = fs.readFileSync("graph.txt").toString();
 module.exports.drawGraph = (coordinate) => {
   const mapLine = map.split("\r\n");
   const mapSplit = mapLine.map((line) => line.split(""));
-  const x = 2 * coordinate[0] + 2;
+  const x = 3 * coordinate[0] + 3;
   const y = 24 - coordinate[1];
   mapSplit[y][x] = "💀";
   const graph = mapSplit.map((split) => split.join("")).join("\n");
