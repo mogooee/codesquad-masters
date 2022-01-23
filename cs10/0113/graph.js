60bd2bd78bed45621a0c6ba0813a0f30c460fe8b
mogooee <mogoo22@naver.com>
2022-01-24 00:35:02 +0900

    update: 여러 개의 점을 출력

--- cs10/0113/graph.js
+++ cs10/0113/graph.js
@@ -4,9 +4,11 @@ const map = fs.readFileSync("graph.txt").toString();
 module.exports.drawGraph = (coordinate) => {
   const mapLine = map.split("\r\n");
   const mapSplit = mapLine.map((line) => line.split(""));
-  const x = 3 * coordinate[0] + 3;
-  const y = 24 - coordinate[1];
-  mapSplit[y][x] = "💀";
+  coordinate.forEach((e) => {
+    const x = 3 * e[0] + 3;
+    const y = 24 - e[1];
+    mapSplit[y][x] = "🍑";
+  });
   const graph = mapSplit.map((split) => split.join("")).join("\n");
   return graph;
 };
