8524ff28173843b8bf439d2e15fc0f065ab0f4ba
mogooee <mogoo22@naver.com>
2022-02-02 22:01:33 +0900

    update: 영업마감

--- cs10/cs08/cafe.js
+++ cs10/cs08/cafe.js
@@ -1,4 +1,5 @@
 const readline = require("readline");
+const EventEmitter = require("events");
 const { Queue } = require("./queue");
 const { Cashier } = require("./cashier");
 const { Manager } = require("./manager");
@@ -6,36 +7,64 @@ const { Barista } = require("./barista");
 const { menu } = require("./menu");
 const { input } = require("./input");
 
-class Cafe {
+class Cafe extends EventEmitter {
   constructor(baristaNum) {
-    this.orderQueue = new Queue();
-    this.cashier = new Cashier(this.orderQueue, menu);
-    this.manager = new Manager("_", this.orderQueue);
+    super();
+    this.customer = [];
     this.barista = [];
     this.baristaNum = baristaNum;
+    this.setBarista();
+    this.orderQueue = new Queue();
+    this.cashier = new Cashier(this.orderQueue, this.customer);
+    this.manager = new Manager(
+      this.barista,
+      this.orderQueue,
+      menu,
+      this.customer,
+      this.baristaNum
+    );
     this.rl = readline.createInterface({
       input: process.stdin,
       output: process.stdout,
       prompt: ">> ",
     });
-    this.init();
+    this.setEvent();
   }
 
-  init() {
+  setBarista() {
     for (let i = 0; i < this.baristaNum; i++) {
-      this.barista.push(new Barista("_", menu));
-      this.barista[i].manager = this.manager;
+      this.barista.push(new Barista(menu));
     }
-    this.manager.barista = this.barista;
+  }
+
+  setEvent() {
+    this.manager.on("quit", () => {
+      this.close();
+    });
   }
 
   open() {
     this.printOpeningMessage();
-    this.rl.on("line", (line) => {
-      const [consumer, ...orders] = input(line);
-      this.cashier.takeOrder(consumer, orders);
-    });
+    this.rl
+      .on("line", (line) => {
+        this.quit = 0;
+        const [customer, ...orders] = input(line);
+        this.cashier.takeOrder(customer, orders);
+      })
+      .on("close", () => {
+        process.exit();
+      });
     this.manager.checkOrderQueue();
+  }
+
+  close() {
+    this.quit = 1;
+    setTimeout(() => {
+      if (this.quit) {
+        this.printClosingMessage();
+        this.rl.close();
+      }
+    }, 3000);
   }
 
   printOpeningMessage() {
@@ -43,16 +72,10 @@ class Cafe {
       `\n🐻‍❄️  Welcome to DOTORI's Cafe  🐻‍❄️\n>> 메뉴 = 1. 아메리카노(3s) 2. 돌체라떼(5s) 3. 민트초코칩프라푸치노(10s)\n>> 주문할 음료를 입력하세요. 예) 아메리카노 2개 => 1:2\n`
     );
   }
+
+  printClosingMessage() {
+    console.log(`\n============ 영업을 마감합니다 ============\n`);
+  }
 }
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-rl.question("How many baristas would you like to set up?", (baristaNum) => {
-  console.log(`There are ${baristaNum} baristas.`);
-  rl.close();
-  const cafe = new Cafe(baristaNum);
-  cafe.open();
-});
+module.exports = { Cafe };
