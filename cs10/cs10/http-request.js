bd723527c94e4370d4171ee1e64b830ae2a31de1
연 <mogoo22@naver.com>
2022-02-11 11:55:07 +0900

    update: http-request.js

---
+++ cs10/cs10/http-request.js
@@ -0,0 +1,84 @@
+const url = "www.disney.co.kr";
+const dns = require("dns");
+const net = require("net");
+
+class HttpRequest {
+  constructor(url, method = "GET") {
+    this.url = url;
+    this.method = method;
+    this.requestMsg = "";
+    this.socket = new net.Socket();
+    this.ip;
+    this.port = 80;
+    this.init();
+  }
+  init() {
+    this.makeRequestLine();
+    this.makeHeader();
+  }
+  makeRequestLine() {
+    return `${this.method} / HTTP/1.1\n`;
+  }
+  makeHeader() {
+    return `Host: ${this.url}`; //필수헤더?
+  }
+  returnRequestMessage() {
+    return (this.requestMsg =
+      this.makeRequestLine() + this.makeHeader() + "\r\n\r\n");
+  }
+  searchDNS() {
+    return new Promise((resolve, reject) => {
+      dns.lookup(url, (err, addresses, family) => {
+        this.ip = addresses;
+        resolve();
+      });
+    });
+  }
+  sendRequestMsg2TCP() {
+    return this.socket.connect(this.port, this.ip, () => {
+      this.socket.write(this.requestMsg);
+    });
+  }
+  responseTCP() {
+    return this.socket.on("data", (serverData) => {
+      this.printServeData(serverData);
+      this.socket.destroy();
+    });
+  }
+  closeSocket() {
+    return this.socket.on("close", () => {
+      this.printCloseMsg();
+    });
+  }
+  request2Server() {
+    this.printSearchingMsg();
+    return this.searchDNS()
+      .then(() => {
+        this.sendRequestMsg2TCP();
+      })
+      .then(() => {
+        this.responseTCP();
+        this.printConnMsg();
+      })
+      .then(() => {
+        this.closeSocket();
+      });
+  }
+  printSearchingMsg() {
+    return console.log("DNS Lookup...");
+  }
+  printConnMsg() {
+    return console.log(`TCP Connection: ${this.ip}, ${this.port}`);
+  }
+  printServeData(data) {
+    return console.log(`[client] received data from server: ${data}`);
+  }
+  printCloseMsg() {
+    return console.log(`[client] connection closed`);
+  }
+}
+
+const httpRequest = new HttpRequest(url);
+httpRequest.returnRequestMessage();
+httpRequest.searchDNS();
+httpRequest.request2Server();
